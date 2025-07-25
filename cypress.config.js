500176684f0f1500276fea9601dfc4e8849ffcb1
Saingyouth Yem <yemsaingyouth@gmail.com>
2025-07-25 04:28:07 -0700

    Update with gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,32 @@
+const fs = require('fs');
 const { defineConfig } = require("cypress");
-const localEnv = require('./config/local.json');
-const ciEnv = require('./config/ci.json');
 
 const environment = process.env?.environment;
 const isLocal = environment === 'local' || environment == undefined;
 
+const readConfigFile = (fileName) => {
+  if (fs.existsSync(fileName)) {
+    return JSON.parse(fs.readFileSync(fileName, 'utf8'));
+  } else {
+    return {};
+  }
+}
+const cypressEnv = readConfigFile(isLocal ? 'config/local.json' : 'config/ci.json');
+
+const users = cypressEnv?.users;
+const extractedUsers = [];
+
+users?.forEach((user) => {
+  Object.keys(user).forEach((key) => {
+    extractedUsers[key] = user[key];
+  })
+});
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    env: isLocal ? localEnv : ciEnv,
+    env: { ...cypressEnv, ...extractedUsers }
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io');
-    console.log(Cypress.env('user_name'));
-    console.log(Cypress.env('password'));
+    console.log(Cypress.env());
   })
 })
